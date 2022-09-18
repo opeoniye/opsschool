abb4fcef4ce33b2295693a0cce81f6749c9f37f1
Samuel John <opeoniye@gmai.com>
2022-09-18 03:56:35 +0100

    assignment ready for submission

--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 #add fix to exercise3 here
+sed -i '32 s/^/#/' /etc/apache2/sites-enabled/000-default.conf
+sed -i '33 s/^/#/' /etc/apache2/sites-enabled/000-default.conf
+sed -i '34 s/^/#/' /etc/apache2/sites-enabled/000-default.conf
+sudo systemctl restart apache2
--- materials/vagrant/fixScripts/exercise4-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+sudo su 
+echo "192.168.60.10 server1" >> /etc/hosts
+su vagrant
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+ssh server1
